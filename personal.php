8eb8747f004ea431b752110b7f6cab876acfaa41
Xzoguy <guyjiru@gmail.com>
2018-11-21 01:05:34 +0700

    Add files via upload

---
+++ personal.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<title>Plern Plern</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+.w3-sidebar a {font-family: "Roboto", sans-serif}
+body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
+input[type=text], select, textarea {
+    width: 40%;
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+
+}
+</style>
+<head>
+</head>
+<body>
+<!-- Sidebar -->
+<div class="w3-sidebar w3-black w3-bar-block" style="width:15%">
+  <h3 class="w3-bar-item">Menu</h3>
+  <a href="#" class="w3-bar-item w3-button">Personal Information</a>
+  <a href="#" class="w3-bar-item w3-button">Link 2</a>
+  <a href="#" class="w3-bar-item w3-button">Link 3</a>
+</div>
+<div style="margin-left:15%">
+
+  <div class="w3-container w3-margin">
+  <form class="w3-margin">
+  <h1><center>Personal Information</center></h1><br>
+    Username:<br>
+    <input type="text" name="username" class="w3-margin"><br>
+    Password:<br>
+    <input type="text" name="password" class="w3-margin"><br>
+  First name:<br>
+    <input type="text" name="firstname" class="w3-margin"><br>
+    Last name:<br>
+    <input type="text" name="lastname" class="w3-margin"><br>
+  ID CARD:<br>
+  <input type="text" name="id card" class="w3-margin"><br>
+  Address:<br>
+  <input type="text" name="address" class="w3-margin"><br>
+  Telephone number: <br>
+  <input type="text" name="tel" class="w3-margin"><br>
+  E-mail:<br>
+  <input type="text" name="email" class="w3-margin"><br>
+
+    <input type="submit" value="Submit" class="w3-margin w3-button w3-red w3-hover-black">
+    <a class="w3-button w3-red w3-hover-black">Cancel</a>
+  </form>
+  </div>
+
+</body>
+</html>
